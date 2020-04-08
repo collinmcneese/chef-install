46a6863cb3b1b1dbf226c4bc3b0368884df4a051
Collin McNeese <cmcneese@chef.io>
2020-04-08 13:24:16 -0500

    windows hacks for testing
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ const exec = require('@actions/exec')
 async function main() {
   try {
     // Get the variables we care about
-    const channel = core.getInput('channel') || 'stable';
-    const project = core.getInput('project') || 'chef-workstation';
+    // const channel = core.getInput('channel') || 'stable';
+    // const project = core.getInput('project') || 'chef-workstation';
     const version = core.getInput('version');
     const omnitruckUrl = core.getInput('omnitruckUrl') || 'omnitruck.chef.io';
     // Create the args that the bash script will need
-    var channelParam = `-c ${channel}`
-    var projectParam = `-P ${project}`
+    // var channelParam = `-c ${channel}`
+    // var projectParam = `-P ${project}`
     if (version) {
       versionParam = `-v ${version}`
     }
     else {
       versionParam = ''
     }
-    await exec.exec(`curl -L https://${omnitruckUrl}/install.sh -o chefDownload.sh`)
-    await exec.exec(`sudo chmod +x chefDownload.sh`)
-    await exec.exec(`sudo ./chefDownload.sh ${channelParam} ${projectParam} ${versionParam}`)
-    await exec.exec(`rm -f chefDownload.sh`)
+    // await exec.exec(`curl -L https://${omnitruckUrl}/install.sh -o chefDownload.sh`)
+    // await exec.exec(`sudo chmod +x chefDownload.sh`)
+    // await exec.exec(`sudo ./chefDownload.sh ${channelParam} ${projectParam} ${versionParam}`)
+    // await exec.exec(`rm -f chefDownload.sh`)
+    await exec.exec(`. { iwr -useb https://${omnitruckUrl}/install.ps1 } | iex; install`)
   } catch (error){
     core.setFailed(error.message);
   }
