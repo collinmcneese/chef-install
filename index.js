79a738f0d094c4be29e6310448995773deb2498f
Collin McNeese <cmcneese@chef.io>
2020-04-08 14:02:57 -0500

    windows hacks for testing
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ async function main() {
     // await exec.exec(`sudo chmod +x chefDownload.sh`)
     // await exec.exec(`sudo ./chefDownload.sh ${channelParam} ${projectParam} ${versionParam}`)
     // await exec.exec(`rm -f chefDownload.sh`)
-    await exec.exec(`powershell.exe -ExecutionPolicy -command { get-command Invoke-WebRequest }`)
-    await exec.exec(`powershell.exe -ExecutionPolicy -command { Invoke-WebRequest -outfile chef-install.ps1 -uri https://${omnitruckUrl}/install.ps1 }`)
-    await exec.exec(`powershell.exe -ExecutionPolicy -command { get-command Get-ChildItem }`)
-    await exec.exec(`powershell.exe -ExecutionPolicy -command { Get-ChildItem }`)
-    await exec.exec(`powershell.exe -ExecutionPolicy -command { .\\chef-install.ps1 }`)
+    await exec.exec(`powershell.exe -ExecutionPolicy Bypass -command { get-command Invoke-WebRequest }`)
+    await exec.exec(`powershell.exe -ExecutionPolicy Bypass -command { Invoke-WebRequest -outfile chef-install.ps1 -uri https://${omnitruckUrl}/install.ps1 }`)
+    await exec.exec(`powershell.exe -ExecutionPolicy Bypass -command { get-command Get-ChildItem }`)
+    await exec.exec(`powershell.exe -ExecutionPolicy Bypass -command { Get-ChildItem }`)
+    await exec.exec(`powershell.exe -ExecutionPolicy Bypass -command { .\\chef-install.ps1 }`)
   } catch (error){
     core.setFailed(error.message);
   }
